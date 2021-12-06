6d52ecf11c349f47f510c5da6f7d2210f906a032
Ethan Perkins <ethanperkinspro@gmail.com>
2021-12-06 10:50:04 -0700

    Initial Commit

---
+++ Hype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hype
+//
+//  Created by Ethan Perkins on 12/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
